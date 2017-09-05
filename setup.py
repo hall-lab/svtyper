d8201297d4f57de3bf1c3816822562d8c2fd0d4e
indraniel <indraniel@gmail.com>
2017-09-05 17:23:56 -0500

    + introduce a first-pass setup.py
      - includes both the setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import svtyper
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='svtyper',
+    version=svtyper.__version__,
+    description='Bayesian genotyper for structural variants',
+    long_description=readme,
+    author=svtyper.__author__,
+    author_email='colbychiang@wustl.edu',
+    license=license,
+    url='https://github.com/hall-lab/svtyper',
+    install_requires=[
+        'pysam>=0.8.1',
+    ],
+    entry_points='''
+        [console_scripts]
+        svtyper=svtyper:main
+    ''',
+    packages=find_packages(exclude=('tests', 'etc')),
+    include_package_data=True,
+)
+
