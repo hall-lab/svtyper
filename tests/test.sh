cdc4be027e20fa8fecfe112f29448406f3b57b3e
indraniel <indraniel@gmail.com>
2017-09-06 13:30:42 -0500

    + update test.sh
      - based on new test data location and conversion of the svtyper script
        to a library

--- tests/test.sh
+++ tests/test.sh
@@ -5,20 +5,20 @@ set -e
 # # make truth set and sort the BAMs
 # BAM=/gscmnt/gc2802/halllab/sv_aggregate/MISC/realigned_BAMs/NA12878/NA12878.bam
 # BAM_BASE=`basename $BAM .bam`
-# ../svtyper \
-#     -i example.vcf \
+# python -m svtyper.core \
+#     -i data/example.vcf \
 #     -B $BAM \
-#     -l $BAM_BASE.bam.json \
-#     -w $BAM_BASE.target_loci.bam \
-#     > example.gt.vcf
+#     -l data/$BAM_BASE.bam.json \
+#     -w data/$BAM_BASE.target_loci.bam \
+#     > data/example.gt.vcf
 #
-# sambamba sort NA12878.target_loci.bam
+# sambamba sort data/NA12878.target_loci.bam
 
 # run test
-../svtyper.py \
-    -i example.vcf \
-    -B NA12878.target_loci.sorted.bam \
-    -l NA12878.bam.json \
+python -m svtyper.core \
+    -i data/example.vcf \
+    -B data/NA12878.target_loci.sorted.bam \
+    -l data/NA12878.bam.json \
     > out.vcf
 
-diff -I '^##fileDate=' example.gt.vcf out.vcf
+diff -I '^##fileDate=' data/example.gt.vcf out.vcf
